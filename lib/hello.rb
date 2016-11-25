cf72bb44918488b59a96ff05811d24ae5d3803de
Vic Lam <lam.vic.k@gmail.com>
2016-11-25 22:40:15 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
-def hello_t
-
+def hello_t(formacion)
+  i = 0
+  while i < formacion.length
+    yield formacion[i]
+    i = i + 1
+  end
+  formacion
 end
 
-# call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.start_with?("T")
+        puts "Hi, #{name}"
+    end
+end
